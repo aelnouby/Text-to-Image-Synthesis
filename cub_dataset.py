ec4bd7a8d508bc93eff35e0cf826c7f315f96d5a
aelnouby <alaaelnouby@gmail.com>
2017-11-04 20:43:23 -0400

    Dataset class implementation for CUB dataset, sampling with right, wrong images and text embeddings

---
+++ cub_dataset.py
@@ -0,0 +1,85 @@
+import os
+import io
+from torch.utils.data import Dataset, DataLoader
+import h5py
+import numpy as np
+import pdb
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import pdb
+
+class CUBDataset(Dataset):
+
+    def __init__(self, datasetFile, transform=None, split=0):
+        self.datasetFile = datasetFile
+        self.transform = transform
+        self.dataset = None
+        self.dataset_keys = None
+        self.split = 'train' if split == 0 else 'valid' if split == 1 else 'test'
+        self.h5py2int = lambda x: int(np.array(x))
+
+    def __len__(self):
+        f = h5py.File(self.datasetFile, 'r')
+        self.dataset_keys = [str(k) for k in f[self.split].keys()]
+        length = len(f[self.split])
+        f.close()
+
+        return length
+
+    def __getitem__(self, idx):
+        if self.dataset is None:
+            self.dataset = h5py.File(self.datasetFile, mode='r')
+            self.dataset_keys = [str(k) for k in self.dataset[self.split].keys()]
+
+        example_name = self.dataset_keys[idx]
+        example = self.dataset[self.split][example_name]
+
+        # pdb.set_trace()
+
+        right_image = bytes(np.array(example['img']))
+        right_embed = np.array(example['embeddings'], dtype=float)
+        wrong_image = bytes(np.array(self.find_wrong_image(example['class'])))
+
+        right_image = Image.open(io.BytesIO(right_image)).resize((64, 64))
+        wrong_image = Image.open(io.BytesIO(wrong_image)).resize((64, 64))
+
+        right_image = self.validate_image(right_image)
+        wrong_image = self.validate_image(wrong_image)
+
+        right_embed = right_embed[np.random.randint(right_embed.shape[0])]
+
+        sample = {
+                'right_image': torch.FloatTensor(right_image),
+                'right_emed': torch.FloatTensor(right_embed),
+                'wrong_image': torch.FloatTensor(wrong_image)
+                 }
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def find_wrong_image(self, category):
+        idx = np.random.randint(len(self.dataset_keys))
+        example_name = self.dataset_keys[idx]
+        example = self.dataset[self.split][example_name]
+        _category = example['class']
+
+        if _category != category:
+            return example['img']
+
+        return self.find_wrong_image(category)
+
+
+    def validate_image(self, img):
+        img = np.array(img, dtype=float)
+        if len(img.shape) < 3:
+            rgb = np.empty((64, 64, 3), dtype=np.float32)
+            rgb[:, :, 0] = img
+            rgb[:, :, 1] = img
+            rgb[:, :, 2] = img
+            img = rgb
+
+        return img.transpose(2, 0, 1)
+
