61bafa12e262a9223d1cd1231f304a1e93f6f38b
aelnouby <alaaelnouby@gmail.com>
2017-11-09 11:14:33 -0500

    Adding command line argument options

--- runtime.py
+++ runtime.py
@@ -1,4 +1,12 @@
 from trainer import Trainer
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-vanilla", default=False, action='store_true')
+args = parser.parse_args()
 trainer = Trainer()
-trainer.train()
+
+if args.vanilla:
+    trainer.train_vanilla_gan()
+else:
+    trainer.train()
