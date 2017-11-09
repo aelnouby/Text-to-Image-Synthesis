00236db36c1f4b55626b604dba2565416dc8d4c1
aelnouby <alaaelnouby@gmail.com>
2017-11-09 11:13:31 -0500

    Fixing batchnorm, adding label smoothing implementation

--- utils.py
+++ utils.py
@@ -9,16 +9,23 @@ class Concat_embed(nn.Module):
         super(Concat_embed, self).__init__()
         self.projection = nn.Sequential(
             nn.Linear(in_features=embed_dim, out_features=projected_embed_dim),
-            # nn.BatchNorm1d(),
+            nn.BatchNorm1d(num_features=projected_embed_dim),
             nn.LeakyReLU(negative_slope=0.2, inplace=True)
             )
 
     def forward(self, inp, embed):
-        try:
-            projected_embed = self.projection(embed)
-            replicated_embed = projected_embed.repeat(4, 4, 1, 1).permute(2,  3, 0, 1)
-            hidden_concat = torch.cat([inp, replicated_embed], 1)
-        except:
-            pdb.set_trace()
+        projected_embed = self.projection(embed)
+        replicated_embed = projected_embed.repeat(4, 4, 1, 1).permute(2,  3, 0, 1)
+        hidden_concat = torch.cat([inp, replicated_embed], 1)
 
         return hidden_concat
+
+
+class Utils(object):
+    def smooth_label(tensor, offset):
+        indices = np.random.choice(range(len(tensor)), int(len(tensor) / 3.0))
+        prob = np.random.rand(len(indices))
+
+        tensor[indices] = tensor[indices] + (offset * prob)
+
+        return tensor
