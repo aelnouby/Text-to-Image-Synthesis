a874a2a1bd16b6d90bdc2aa899ba998dadbcd4d5
aelnouby <alaaelnouby@gmail.com>
2017-11-10 11:57:03 -0500

    Implement minibatch_discrimination module

--- utils.py
+++ utils.py
@@ -21,6 +21,32 @@ class Concat_embed(nn.Module):
         return hidden_concat
 
 
+class minibatch_discriminator(nn.Module):
+    def __init__(self, num_channels, B_dim, C_dim):
+        super(minibatch_discriminator, self).__init__()
+        self.B_dim = B_dim
+        self.C_dim =C_dim
+        self.num_channels = num_channels
+        T_init = torch.randn(num_channels * 4 * 4, B_dim * C_dim) * 0.1
+        self.T_tensor = nn.Parameter(T_init, requires_grad=True)
+
+    def forward(self, inp):
+        inp_flat = inp.view(-1, self.num_channels * 4 * 4)
+        M = inp_flat.mm(self.T_tensor)
+        M = M.view(-1, self.B_dim, self.C_dim)
+
+        op1 = M.unsqueeze(3)
+        op2 = M.permute(1, 2, 0).unsqueeze(0)
+
+        output = torch.sum(torch.abs(op1 - op2), 2)
+        output = torch.sum(torch.exp(-output), 2)
+        output = output.view(M.size(0), -1, 4, 4)
+
+        output = torch.cat((inp, output), 1)
+
+        return output
+
+
 class Utils(object):
     def smooth_label(tensor, offset):
         indices = np.random.choice(range(len(tensor)), int(len(tensor) / 2.0))
