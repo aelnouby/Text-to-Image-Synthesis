069ab010fcfec3cd8cab16978b0752a229c08f86
aelnouby <alaaelnouby@gmail.com>
2017-11-21 18:26:41 -0500

    Adding new command line options (gan type, lr, diter)

--- runtime.py
+++ runtime.py
@@ -2,11 +2,13 @@ from trainer import Trainer
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-vanilla", default=False, action='store_true')
+parser.add_argument("--type", default='gan')
+parser.add_argument("--lr", default=0.00005, type=float)
+parser.add_argument("--diter", default=5, type=int)
+parser.add_argument("-cls", default=False, action='store_true')
 args = parser.parse_args()
-trainer = Trainer()
 
-if args.vanilla:
-    trainer.train_gan()
-else:
-    trainer.train_gan_cls()
+trainer = Trainer(type=args.type, lr= args.lr, diter=args.diter)
+trainer.train(args.cls)
+
+
