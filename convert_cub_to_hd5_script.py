421aa3972898234f62594a4bf6abf7b9b54c21b6
aelnouby <alaaelnouby@gmail.com>
2017-11-29 12:22:44 -0500

    Adding the text to the dataset

--- convert_cub_to_hd5_script.py
+++ convert_cub_to_hd5_script.py
@@ -10,10 +10,11 @@ import io
 import pdb
 
 with open('config.yaml', 'r') as f:
-    config = yaml.load(f)
+	config = yaml.load(f)
 
 images_path = config['birds_images_path']
 embedding_path = config['birds_embedding_path']
+text_path = config['birds_text_path']
 datasetDir = config['birds_dataset_path']
 
 val_classes = open(config['val_split_path']).read().splitlines()
@@ -35,16 +36,26 @@ for _class in sorted(os.listdir(embedding_path)):
 		split = test
 
 	data_path = os.path.join(embedding_path, _class)
-	for example in sorted(glob(data_path + "/*.t7")):
+	txt_path = os.path.join(text_path, _class)
+	for example, txt_file in zip(sorted(glob(data_path + "/*.t7")), sorted(glob(txt_path + "/*.txt"))):
 		example_data = load_lua(example)
 		img_path = example_data['img']
 		embeddings = example_data['txt'].numpy()
 		example_name = img_path.split('/')[-1][:-4]
 
+		f = open(txt_file, "r")
+		txt = f.readlines()
+		f.close()
+
 		img_path = os.path.join(images_path, img_path)
 		img = open(img_path, 'rb').read()
 
-		embeddings = embeddings[np.random.choice(range(10), 5)]
+		txt_choice = np.random.choice(range(10), 5)
+
+		embeddings = embeddings[txt_choice]
+		txt = np.array(txt)
+		txt = txt[txt_choice]
+		dt = h5py.special_dtype(vlen=str)
 
 		for c, e in enumerate(embeddings):
 			ex = split.create_group(example_name + '_' + str(c))
@@ -52,6 +63,7 @@ for _class in sorted(os.listdir(embedding_path)):
 			ex.create_dataset('img', data=np.void(img))
 			ex.create_dataset('embeddings', data=e)
 			ex.create_dataset('class', data=_class)
+			ex.create_dataset('txt', data=txt[c].astype(object), dtype=dt)
 
 		print(example_name)
 
